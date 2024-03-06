61ce8ad5f532ac5177bf47100eebbde2c46424d7
mesqta <meskita@proton.me>
2024-03-06 12:52:10 -0300

    commit

---
+++ 01 - Abrir Apps/03-Whatsapp-Web.py
@@ -0,0 +1,28 @@
+import pyautogui
+from time import sleep
+'''
+Funções:
+    pyautogui.click() - Clica em algum lugar da tela
+    pyautogui.write() - Escreve um texto
+    pyautogui.press() - Pressiona alguma tecla do teclado
+'''
+
+# Abrir o windows
+pyautogui.press("win")
+# Digita o navegador de acordo com que o pc tem instalado
+pyautogui.write("Brave");sleep(1)
+# Abre o navegador
+pyautogui.press("enter");sleep(1)
+# Coloca o site no campo de busca
+link = "https://web.whatsapp.com/"
+# Coloca o link no campo de pesquisa
+pyautogui.write(link);sleep(2)
+pyautogui.press("enter");sleep(1)
+
+
+'''
+    função sleep:
+        a função sleep  é usada para dar uma pausa na execução do programa por um tempo determinado,
+        permitindo assim que outras operações sejam executadas enquanto ele está dormindo.
+
+'''
---
+++ posição.py
@@ -0,0 +1,11 @@
+import pyautogui
+from time import sleep
+
+sleep(5)
+print(pyautogui.position())
+
+'''
+Função do pyautogui.position()
+    Ele pega a posição do mouse em um time de 5 segundos e imprime na tela
+    para voce colocar o cursor no local correto
+'''
